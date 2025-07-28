1e8f7d5ec6fcbb0708b960974a3c9ee8b1903a48
ELNYELISIS <nyellove1998.com>
2025-07-28 17:51:01 -0500

    ProyectoBonillaFrontendFinal

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -1,11 +1,13 @@
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-const openaiApiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
-const backendUrl = "http://localhost:3000/api/v1";
+const backendUrl =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_BACKEND_URL_DEV
+    : import.meta.env.VITE_BACKEND_URL_PROD;
 
 const frontendUrl =
   import.meta.env.MODE === "development"
     ? import.meta.env.VITE_URL_BASE_LOCAL
     : import.meta.env.VITE_URL_BASE;
 
-export { clientId, openaiApiKey, backendUrl, frontendUrl };
+export { clientId, backendUrl, frontendUrl };
